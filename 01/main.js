d373e711525a8e49e0e0708b1d3e6bcf37fc10ed
Erika Flores <foglzerika@gmail.com>
2022-01-16 07:03:29 -0600

    update | Update code challenge 01

--- 01/main.js
+++ 01/main.js
@@ -1,1 +1,21 @@
 // your code goes here
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'},
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo'}
+];
+
+const contarOvejas = (ovejas) =>{
+    return `Vamos a contar ${ovejas} ovejas`
+}//contarOvejas
+console.log( contarOvejas(2));
+
+const ovejasFiltradas = contarOvejas(ovejas)
+// Descomenta esta línea
+//console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
